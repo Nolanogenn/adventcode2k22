0c06c6cf75ec3d340c2f6d0e9453d125a74a79db
Gennaro Nolano <nolanogenn@gmail.com>
2022-12-07 10:16:51 +0100

    day7

--- day6/puzzle2/solvepuzzle.py
+++ day6/puzzle2/solvepuzzle.py
@@ -1,14 +1,17 @@
+import time
+
+start_time = time.time()
 data = open('input.txt').read()
-data = data.replace('\n', '')
 
 buffer = []
 for i, char in enumerate(data):
     if len(buffer) == 14:
-        if len(list(set(buffer))) == 14:
-            print(buffer)
+        if len(set(buffer)) == 14:
             print(i)
             break
         else:
             buffer.pop(0)
     buffer.append(char)
+
+print("--- %s seconds ---" % (time.time() - start_time))
 
